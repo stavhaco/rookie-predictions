d31218b14cdae4f02cc7340d9471b5d4d8b753ab
Stav Hacohen <stavhaco@gmail.com>
2018-04-25 10:48:55 +0300

    24/02/18 update

--- rookie_statistical_analysis.py
+++ rookie_statistical_analysis.py
@@ -13,8 +13,7 @@ from sklearn.ensemble import RandomForestRegressor
 
 
 def prepare_data(data):
-    data["star"]=np.where(data["BEST EFF"]<=100,1,0)
-    #dropping corrolated features and unnecceserry ones
+    data["star"]=np.where(data["BEST EFF"]<=70,1,0)
     #dropping rank features
     features_drop_ranks = ['GP_RANK', 'W_RANK', 'L_RANK', 'W_PCT_RANK', 'MIN_RANK', 'FGM_RANK', 'FGA_RANK', 'FG_PCT_RANK', 'FG3M_RANK', 'FG3A_RANK', 'FG3_PCT_RANK', 'FTM_RANK', 'FTA_RANK', 'FT_PCT_RANK', 'OREB_RANK', 'DREB_RANK', 'REB_RANK', 'AST_RANK', 'TOV_RANK', 'STL_RANK', 'BLK_RANK', 'BLKA_RANK', 'PF_RANK', 'PFD_RANK', 'PTS_RANK', 'PLUS_MINUS_RANK', 'NBA_FANTASY_PTS_RANK', 'DD2_RANK', 'TD3_RANK']
     #dropping corolated features according to heat map
@@ -32,7 +31,7 @@ def preform_regression(data_train,data_test):
     X_train = data_train.drop(["star"],axis=1)
     #cols = ['TEAM_ID', 'AGE', 'GP', 'MIN', 'FG_PCT', 'FG3_PCT', 'FT_PCT', 'OREB', 'DREB', 'AST', 'TOV', 'STL', 'BLK',
             #'BLKA', 'PF', 'PFD', 'PTS', 'DD2', 'TD3']
-    cols = ['TEAM_ID','AGE','GP','FG_PCT','AST','OREB','PTS']
+    cols = ['TEAM_ID','AGE','GP','AST','OREB','PTS']
     X_train = data_train[cols]
     y_train = data_train["star"]
     X_test = data_test[cols]
@@ -47,7 +46,7 @@ def preform_regression(data_train,data_test):
     conf_matrix = confusion_matrix(y_pred_binary,y_test)
     print conf_matrix
     true_neg,false_pos,false_neg,true_pos = conf_matrix[1][1],conf_matrix[1][0],conf_matrix[0][1],conf_matrix[0][0]
-    print "prediction of not-great players "+str(float(true_neg)/(true_neg+false_neg))
+    print "prediction of not-great players "+str(round(float(true_neg)/(true_neg+false_neg),2))
     print "prediction of great players "+str(round(float(true_pos)/(true_pos+false_pos),2))
 
 def preform_rand_forest(data_train,data_test):
@@ -65,7 +64,7 @@ def preform_rand_forest(data_train,data_test):
     conf_matrix = confusion_matrix(y_pred_binary,y_test)
     print conf_matrix
     true_neg,false_pos,false_neg,true_pos = conf_matrix[1][1],conf_matrix[1][0],conf_matrix[0][1],conf_matrix[0][0]
-    print "prediction of not-great players "+str(float(true_neg)/(true_neg+false_neg))
+    print "prediction of not-great players "+str(round(float(true_neg)/(true_neg+false_neg),2))
     print "prediction of great players "+str(round(float(true_pos)/(true_pos+false_pos),2))
 #print rookie_data_train.groupby('TEAM_ID').star.value_counts()
 #print rookie_data_train[['TEAM_ID', 'star']].groupby(['TEAM_ID'], as_index=False).mean()
